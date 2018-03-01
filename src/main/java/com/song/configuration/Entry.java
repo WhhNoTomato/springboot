60f5174f3f2e0c834088c1d5eb9c12d54ce60d38
ichimoRen <36949889@qq.com>
2018-03-01 09:17:38 +0800

    ADD URL

--- src/main/java/com/song/configuration/Entry.java
+++ src/main/java/com/song/configuration/Entry.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * Created by Song on 2017/2/15.
  * 项目启动入口，配置包根路径
@@ -11,7 +16,29 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.song")
 public class Entry {
+
+    public static void browserUrl(String url){
+
+        //判断是否支持Desktop扩展,如果支持则进行下一步
+        if (Desktop.isDesktopSupported()){
+            try {
+                URI uri = new URI(url);
+                Desktop desktop = Desktop.getDesktop(); //创建desktop对象
+                //调用默认浏览器打开指定URL
+                desktop.browse(uri);
+
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                //如果没有默认浏览器时，将引发下列异常
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+        browserUrl("http://localhost:8080/user/index");
         SpringApplication.run(Entry.class, args);
     }
 }
